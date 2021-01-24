b202f2382ccd7ef5893348d933c5b788c9e9a94c
Virtor Hirenko <viktor.hirenko@gmail.com>
2021-01-24 13:14:59 +0200

    add logic for sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 import modals from './moules/modals';
+import sliders from './moules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use stict';
 
     modals();
+    sliders(
+        '.feedback-slider-item',
+        'horizontal',
+        '.main-prev-btn',
+        '.main-next-btn',
+    );
+    sliders('.main-slider-item', 'vertical');
 });
